74ee6d53607fb495e7426d401ff5eca621518781
Sumit Karmakar <amiesumit@gmail.com>
2021-05-23 00:06:21 +0530

    practise

---
+++ app.js
@@ -0,0 +1,29 @@
+/*let num1 = 5;
+let num2 = '5';
+//Equality operator
+//Equality operator check for values, not data types
+// == true, === false
+console.log('== Operator ', num1==num2);
+//typeoff operator
+let data = 600;
+console.log("Datatype of data is:", typeof data); */
+
+
+//ternary operator
+/*let age = 17;
+let status = age >= 18 ? 'Eligible' : 'Not Eligible';
+console.log(status);
+*/
+
+//Switch Case
+/*
+let choice = 'c';
+switch(choice)
+{
+    case 'a': console.log("This is case a");
+        break;
+    case 'b': console.log("This is case b");
+        break;
+    default : console.log("This is default")
+}
+*/
