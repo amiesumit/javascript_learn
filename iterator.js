2177e1c28b69bebed1bb1762478905afd21a14df
Sumit Karmakar <amiesumit@gmail.com>
2021-05-30 22:23:09 +0530

    added for iterators

---
+++ iterator.js
@@ -0,0 +1,20 @@
+//For loop
+for (let i = 0; i<=5 ; i++)
+{
+    console.log(i);
+}
+
+//while loop
+let j = 0;
+while (j<=5)
+{
+    console.log("In while loop: "+j);
+    j++;
+}
+//do-while loop - exit control loop
+let k = 0;
+do{
+    console.log('Do while:' +k);
+    k++;
+}
+while(k<=5);
